d2ba05b1529e027ee645c6fce4bd458a10c48b51
Fernando Rojas <fernandorojas201482@gmail.com>
2022-04-27 17:30:39 -0300

    agrego carpeta tp3

--- TP2/Code/data/pand.py
+++ TP2/Code/data/pand.py
@@ -1,6 +1,12 @@
 import pandas as pd
+import msvcrt
 df = pd.read_csv("dataset.csv", index_col="id")
 print(df)
-df.describe()
-df.head()
-df.tail()
+print()
+print(df.describe())
+print()
+print(df.head())
+print()
+print(df.tail())
+print()
+msvcrt.getch()
